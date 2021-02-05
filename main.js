d43a6d3cd0216913c17eaeb439bac0ed93d0d480
tokaelsayed <tokasayed045@gmail.com>
2021-02-05 18:02:52 +0200

    final project

---
+++ main.js
@@ -0,0 +1,9 @@
+function imgslider(anything)
+    {
+        document.querySelector('.starbucks').src=anything;
+    }
+    function changecirlclecolor(color)
+    {
+const circle=document.querySelector('.circle');
+circle.style.background=color;
+    }
